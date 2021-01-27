885a5efee5b3931ea5615824d3ed8dc674d81869
Ömer Özkan <omerozkan758@hotmail.com>
2021-01-28 01:42:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] key;
+        T2[] value;
+        public MyDictionary()
+        {
+            key = new T1[0];
+            value = new T2[0];
+        }
+        public void Add(T1 ke, T2 val)
+        {
+            T1[] tempKeys = key;
+            T2[] tempVals = value;
+
+            key = new T1[key.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                key[i] = tempKeys[i];
+            }
+            key[key.Length - 1] = ke;
+
+            value = new T2[value.Length + 1];
+            for (int i = 0; i < tempVals.Length; i++)
+            {
+                value[i] = tempVals[i];
+            }
+            value[value.Length - 1] = val;
+        }
+        public T1[] ilk
+        {
+            get { return key; }
+        }
+        public T2[] son
+        {
+            get { return value; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> elementSirala = new MyDictionary<int, string>();
+            elementSirala.Add(1, "Hidrojen");
+            elementSirala.Add(2, "Helyum");
+            elementSirala.Add(3, "Lityum");
+            elementSirala.Add(4, "Berilyum");
+            elementSirala.Add(5, "Bor");
+            elementSirala.Add(6, "Karbon");
+            elementSirala.Add(7, "Azot");
+            elementSirala.Add(8, "Oksijen");
+            elementSirala.Add(9, "Flor");
+            elementSirala.Add(10, "Neon");
+
+            Console.WriteLine(elementSirala.ilk[2]+" "+elementSirala.son[2]);
+        }
+    }
+}
